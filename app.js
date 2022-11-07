5c072dca1f7e1d208e95886ee98587415e375eb0
carraes <carraeshb@gmail.com>
2022-11-07 19:48:03 -0300

    chore: initial app

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+const colors = require('colors')
+
+const notFoundHandlerMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+colors.enable = true
+const port = process.env.PORT || 3000
+
+// ? Middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.use(notFoundHandlerMiddleware)
+app.use(errorHandlerMiddleware)
+
+// ! App
+
+const start = () => {
+  try {
+    app.listen(port, console.log(`Listening on: http://localhost:${port}/`.brightCyan.bold))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
